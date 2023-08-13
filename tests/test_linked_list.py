3074301ce0b6388b64e7439d30e8e0709c917f84
Artem Dedov <artem-dedov2012@yandex.ru>
2023-08-14 02:17:17 +0300

    Added:   Linked list class implementation   Tests for class Linked list

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,26 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    """Тесты для класса linked_list."""
+
+    def test_insert_beginning(self):
+        """Тест для метода insert_beginning."""
+        linked_list = LinkedList()
+        linked_list.insert_beginning({'id': 1})
+        self.assertEqual(linked_list.head.data, {'id': 1})
+
+    def test_insert_end(self):
+        """Тест для метода insert_end."""
+        linked_list = LinkedList()
+        linked_list.insert_at_end({'id': 1})
+        self.assertEqual(linked_list.tail.data, {'id': 1})
+
+    def test_str(self):
+        """Тест для метода __str__."""
+        linked_list = LinkedList()
+        linked_list.insert_beginning({'id': 1})
+        self.assertEqual(str(linked_list), " {'id': 1} -> None")
